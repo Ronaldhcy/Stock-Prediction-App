d67288e344b9cb54d452738c0e5142fa00848075
Ronald Ho <ronaldhcy0522@gmail.com>
2021-12-28 08:44:05 +0800

    Add Neural Prophet as model option for forecast.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+from neuralprophet import NeuralProphet
+from neuralprophet import plot_forecast
 
 START = '2010-01-01'
 TODAY = date.today().strftime('%Y-%m-%d')
@@ -44,6 +46,9 @@ elif period_of_prediction == 'Yearly':
 
 uncertainty_interval = st.slider("Uncertainty interval", min_value = 0.0, max_value = 1.0, value = 0.8)
 
+type_of_model = ('Prophet', 'Neural Prophet')
+model_selected = st.selectbox("Select the model to be used for forecast", type_of_model)
+
 enter_button = st.button('Generate forecast!')
 
 if enter_button:
@@ -74,18 +79,54 @@ if enter_button:
     df_train = data[['Date', 'Close']]
     df_train = df_train.rename(columns={'Date': 'ds', "Close": 'y'})
 
-    m = Prophet(interval_width=uncertainty_interval)
-    m.fit(df_train)
-    future = m.make_future_dataframe(periods = period)
-    forecast = m.predict(future)
+    #For Prophet:
+    if model_selected == 'Prophet':
+        model = Prophet(interval_width=uncertainty_interval)
+        model.fit(df_train)
+        future = model.make_future_dataframe(periods = period)
+        forecast = model.predict(future)      
 
-    st.subheader('Forecast data')
-    st.write(forecast.tail())
+        st.subheader('Forecast data')
+        st.write(forecast.tail())
 
-    st.write('forecast data')
-    fig1 = plot_plotly(m, forecast)
-    st.plotly_chart(fig1)
+        st.write('forecast data')
+        fig1 = plot_plotly(model, forecast)
+        st.plotly_chart(fig1)
 
-    st.write('forecast components')
-    fig2 = m.plot_components(forecast)
-    st.write(fig2)
+        st.write('forecast components')
+        fig2 = model.plot_components(forecast)
+        st.write(fig2)  
+
+    #For Neural Prophet
+    elif model_selected == 'Neural Prophet':
+        model = NeuralProphet()
+        model.fit(df_train, freq='D')
+        future = model.make_future_dataframe(df_train, periods = period)
+        forecast = model.predict(future)
+
+        st.subheader('Forecast data')
+        st.write(forecast.tail())
+
+        st.write('forecast data')
+        fig1 = plot_forecast.plot(forecast)
+        st.plotly_chart(fig1)
+
+        st.write('forecast components')
+        fig2 = model.plot_components(forecast)
+        st.write(fig2)
+
+    #model.fit(df_train, freq='D')
+    #future = model.make_future_dataframe(periods = period)
+    #forecast = model.predict(future)
+    
+    #st.subheader('Forecast data')
+    #st.write(forecast.tail())
+
+    #st.write('forecast data')
+    #fig1 = plot_plotly(model, forecast)
+    #st.plotly_chart(fig1)
+
+    #st.write('forecast components')
+    #fig2 = model.plot_components(forecast)
+    #st.write(fig2)
+    
