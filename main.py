803e07f0db4c9e548ce5dc96cf83bd5c3292caae
Ronald Ho <ronaldhcy0522@gmail.com>
2021-12-29 13:51:01 +0800

    1. Update display with container 2. Add 0700.hk and 0005.hk as new stock options.

--- main.py
+++ main.py
@@ -13,43 +13,45 @@ TODAY = date.today().strftime('%Y-%m-%d')
 
 st.title("Stock Prediction App")
 
+container = st.container()
+
 asset_type = ('Stock', 'Forex', 'Index', 'Crypocurrency')
 
-stocks = ('AAPL', "GOOG", "MSFT", "GME")
+stocks = ('AAPL', "GOOG", "MSFT", "GME", '0700.HK', '0005.HK')
 forex = ('USDJPY=X', 'EURUSD=X', 'GBPUSD=X')
 index = ('^DJI', '^IXIC', '^GSPC', '^HSI')
 crypo_curr = ('BTC-USD', 'ETH-USD', 'BNB-USD')
 period = ('Weekly', 'Monthly', 'Yearly')
 
-selected_asset_type = st.selectbox('Select type of asset to be predicted: ', asset_type)
+selected_asset_type = container.selectbox('Select type of asset to be predicted: ', asset_type)
 
 if selected_asset_type == 'Stock':
-    selected_asset = st.selectbox("Select stock for prediction", stocks)
+    selected_asset = container.selectbox("Select stock for prediction", stocks)
 elif selected_asset_type == 'Forex':
-    selected_asset = st.selectbox("Select forex for prediction", forex)
+    selected_asset = container.selectbox("Select forex for prediction", forex)
 elif selected_asset_type == 'Index':
-    selected_asset = st.selectbox("Select index for prediction", index)
+    selected_asset = container.selectbox("Select index for prediction", index)
 elif selected_asset_type == 'Crypocurrency':
-    selected_asset = st.selectbox("Select crypo for prediction", crypo_curr)   
+    selected_asset = container.selectbox("Select crypo for prediction", crypo_curr)   
 
-period_of_prediction = st.selectbox("Select period of prediction", period) 
+period_of_prediction = container.selectbox("Select period of prediction", period) 
 
 if period_of_prediction == 'Weekly':
-    n_periods = st.slider("Duration of prediction:", 1, 7)
+    n_periods = container.slider("Duration of prediction:", 1, 7)
     period = n_periods
 elif period_of_prediction == 'Monthly':
-    n_periods = st.slider("Duration of prediction:", 7, 365)
+    n_periods = container.slider("Duration of prediction:", 7, 365)
     period = n_periods
 elif period_of_prediction == 'Yearly':
-    n_periods = st.slider("Duration of prediction:", 1, 10)
+    n_periods = container.slider("Duration of prediction:", 1, 10)
     period = n_periods * 365
 
-uncertainty_interval = st.slider("Uncertainty interval", min_value = 0.0, max_value = 1.0, value = 0.8)
+uncertainty_interval = container.slider("Uncertainty interval", min_value = 0.0, max_value = 1.0, value = 0.8)
 
 type_of_model = ('Prophet', 'Neural Prophet')
-model_selected = st.selectbox("Select the model to be used for forecast", type_of_model)
+model_selected = container.selectbox("Select the model to be used for forecast", type_of_model)
 
-enter_button = st.button('Generate forecast!')
+enter_button = container.button('Generate forecast!')
 
 if enter_button:
 
@@ -59,19 +61,19 @@ if enter_button:
         data.reset_index(inplace=True)
         return data
 
-    data_load_state = st.text("Load data...")
+    data_load_state = container.text("Load data...")
     data = load_data(selected_asset)
     data_load_state.text("Loading data...done!")
 
-    st.subheader('Raw data')
-    st.write(data.tail())
+    container.subheader('Raw data')
+    container.write(data.tail())
 
     def plot_raw_data():
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
         fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
         fig.layout.update(title_text='Time Series Data', xaxis_rangeslider_visible=True)
-        st.plotly_chart(fig)
+        container.plotly_chart(fig)
 
     plot_raw_data()
 
@@ -86,16 +88,16 @@ if enter_button:
         future = model.make_future_dataframe(periods = period)
         forecast = model.predict(future)      
 
-        st.subheader('Forecast data')
-        st.write(forecast.tail())
+        container.subheader('Forecast data')
+        container.write(forecast.tail())
 
-        st.write('forecast data')
+        container.write('forecast data')
         fig1 = plot_plotly(model, forecast)
-        st.plotly_chart(fig1)
+        container.plotly_chart(fig1)
 
-        st.write('forecast components')
+        container.write('forecast components')
         fig2 = model.plot_components(forecast)
-        st.write(fig2)  
+        container.write(fig2)  
 
     #For Neural Prophet
     elif model_selected == 'Neural Prophet':
@@ -104,16 +106,16 @@ if enter_button:
         future = model.make_future_dataframe(df_train, periods = period)
         forecast = model.predict(future)
 
-        st.subheader('Forecast data')
-        st.write(forecast.tail())
+        container.subheader('Forecast data')
+        container.write(forecast.tail())
 
-        st.write('forecast data')
+        container.write('forecast data')
         fig1 = plot_forecast.plot(forecast)
-        st.plotly_chart(fig1)
+        container.plotly_chart(fig1)
 
-        st.write('forecast components')
+        container.write('forecast components')
         fig2 = model.plot_components(forecast)
-        st.write(fig2)
+        container.write(fig2)
 
     #model.fit(df_train, freq='D')
     #future = model.make_future_dataframe(periods = period)
